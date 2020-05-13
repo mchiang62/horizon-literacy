6dd5d5c7983aa3389ea46b528e61df07f3e7fb12
Melissa Chiang <mchiang62@gmail.com>
2020-05-13 14:11:28 -0500

    working progress on error message

--- assets/signup.js
+++ assets/signup.js
@@ -4,14 +4,44 @@ function validation () {
 
   var firstName = document.getElementById('inputFirstName').value;
   var lastName = document.getElementById('inputLastName').value;
+  var birthday = document.getElementById('inputBirthday').value;
+  var address = document.getElementById('inputAddress').value;
+  var city = document.getElementById('inputCity').value;
+  var zip = document.getElementById('inputZip').value;
+  var phone = document.getElementById('inputPhone').value;
+  var email = document.getElementById('inputEmail4').value;
 
   if (firstName == "") {
     document.getElementById('fName').innerHTML= "Required";
     return false
-  };
+  }
 
   if (lastName == "") {
     document.getElementById('lName').innerHTML= "Required";
+    return false
+  }
+  if (birthday == "") {
+    document.getElementById('dob').innerHTML= "Required";
+    return false
+  }
+  if (address == "") {
+    document.getElementById('address').innerHTML= "Required";
+    return false
+  }
+  if (city == "") {
+    document.getElementById('city').innerHTML= "Required";
+    return false
+  }
+  if (zip == "") {
+    document.getElementById('zip').innerHTML= "Required";
+    return false
+  }
+  if (phone == "") {
+    document.getElementById('phone').innerHTML= "Required";
+    return false
+  }
+  if (email == "") {
+    document.getElementById('email').innerHTML= "Required";
     return false
   }
 
