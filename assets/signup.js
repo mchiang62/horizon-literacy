c92e879d034069f5833d35156827a0e418222c7f
Melissa Chiang <mchiang62@gmail.com>
2020-05-13 18:54:42 -0500

    made some progress visuals and required fields

--- assets/signup.js
+++ assets/signup.js
@@ -1,53 +1,92 @@
-console.log("hi melissa")
+console.log("hi melissa");
 
-function validation () {
+const form = document.getElementById("signup");
+const firstName = document.getElementById("inputFirstName");
+const lastName = document.getElementById("inputLastName");
+const birthday = document.getElementById("inputBirthday");
+const address = document.getElementById("inputAddress");
+const city = document.getElementById("inputCity");
+const zip = document.getElementById("inputZip");
+const phone = document.getElementById("inputPhone");
+const email = document.getElementById("inputEmail4");
 
-  var firstName = document.getElementById('inputFirstName').value;
-  var lastName = document.getElementById('inputLastName').value;
-  var birthday = document.getElementById('inputBirthday').value;
-  var address = document.getElementById('inputAddress').value;
-  var city = document.getElementById('inputCity').value;
-  var zip = document.getElementById('inputZip').value;
-  var phone = document.getElementById('inputPhone').value;
-  var email = document.getElementById('inputEmail4').value;
+form.addEventListener("submit", function () {
+  event.preventDefault();
 
-  if (firstName == "") {
-    document.getElementById('fName').innerHTML= "Required";
-    return false
-  }
+  checkInputs();
+});
 
-  if (lastName == "") {
-    document.getElementById('lName').innerHTML= "Required";
-    return false
-  }
-  if (birthday == "") {
-    document.getElementById('dob').innerHTML= "Required";
-    return false
-  }
-  if (address == "") {
-    document.getElementById('address').innerHTML= "Required";
-    return false
-  }
-  if (city == "") {
-    document.getElementById('city').innerHTML= "Required";
-    return false
-  }
-  if (zip == "") {
-    document.getElementById('zip').innerHTML= "Required";
-    return false
-  }
-  if (phone == "") {
-    document.getElementById('phone').innerHTML= "Required";
-    return false
-  }
-  if (email == "") {
-    document.getElementById('email').innerHTML= "Required";
-    return false
-  }
+function checkInputs() {
+  {
+    const fnameValue = firstName.value.trim();
+    const lnameValue = lastName.value.trim();
+    const birthdayValue = birthday.value.trim();
+    const addressValue = address.value.trim();
+    const cityValue = city.value.trim();
+    const zipValue = zip.value.trim();
+    const phoneValue = phone.value.trim();
+    const emailValue = email.value.trim();
 
-
-
+    if (fnameValue === "") {
+      // show error message
+      setErrorFor(firstName, "Required");
+    } else {
+      return false
+    }
+  }
 }
 
+function setErrorFor(input, message) {
+  const formGroup = input.parentElement;
+  const span = formGroup.querySelector('span');
 
+  span.innerHTML = message;
 
+  formGroup.className = "form-group error";
+}
+
+// const firstName = document.getElementById('inputFirstName').value;
+// const lastName = document.getElementById('inputLastName').value;
+// const birthday = document.getElementById('inputBirthday').value;
+// const address = document.getElementById('inputAddress').value;
+// const city = document.getElementById('inputCity').value;
+// const zip = document.getElementById('inputZip').value;
+// const phone = document.getElementById('inputPhone').value;
+// const email = document.getElementById('inputEmail4').value;
+
+// if (firstName == "") {
+//   document.getElementById('fName').innerHTML= "Required";
+//   return false
+// }
+
+// if (lastName == "") {
+//   document.getElementById('lName').innerHTML= "Required";
+//   return false
+// }
+// if (birthday == "") {
+//   document.getElementById('dob').innerHTML= "Required";
+//   return false
+// }
+// if (address == "") {
+//   document.getElementById('address').innerHTML= "Required";
+//   return false
+// }
+// if (city == "") {
+//   document.getElementById('city').innerHTML= "Required";
+//   return false
+// }
+// if (zip == "") {
+//   document.getElementById('zip').innerHTML= "Required";
+//   return false
+// }
+// if (phone == "") {
+//   document.getElementById('phone').innerHTML= "Required";
+//   return false
+// }
+// if (email == "") {
+//   document.getElementById('email').innerHTML= "Required";
+//   return false
+// }
+
+//   function validation () {
+// }
